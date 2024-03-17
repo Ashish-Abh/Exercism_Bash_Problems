f3e5b0a17290b8cc1764d03aa7a4053c4c507067
Ashish Bhattarai <146844760+Ashish-Abh@users.noreply.github.com>
2024-03-17 20:41:38 +0000

    Update Two Fer.sh

--- Two Fer.sh
+++ Two Fer.sh
@@ -9,5 +9,6 @@
 #If your friend doesn't like cookies, you give the cookie to the next person in line at the bakery. Since you don't know their name, you will say you instead.
 #One for you, one for me.
 
+# <Solution>
 #!/usr/bin/env bash
 echo "One for ${1:-you}, one for me."
