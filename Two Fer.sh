ec6e7351818decbf52b56fa1ec26a7d75ed11103
Ashish Bhattarai <146844760+Ashish-Abh@users.noreply.github.com>
2024-03-17 20:40:09 +0000

    Update Two Fer.sh

--- Two Fer.sh
+++ Two Fer.sh
@@ -1,2 +1,13 @@
+<b>Introduction</b>
+<p>In some English accents, when you say "two for" quickly, it sounds like "two fer". Two-for-one is a way of saying that if you buy one, you also get one for free. So the phrase "two-fer" often implies a two-for-one offer.
+Imagine a bakery that has a holiday offer where you can buy two cookies for the price of one ("two-fer one!"). You go for the offer and (very generously) decide to give the extra cookie to a friend. </p>
+
+<b>Instructions</b>
+<p>Your task is to determine what you will say as you give away the extra cookie.
+If your friend likes cookies, and is named Do-yun, then you will say:
+One for Do-yun, one for me.
+If your friend doesn't like cookies, you give the cookie to the next person in line at the bakery. Since you don't know their name, you will say you instead.
+One for you, one for me. </p>
+
 #!/usr/bin/env bash
 echo "One for ${1:-you}, one for me."
